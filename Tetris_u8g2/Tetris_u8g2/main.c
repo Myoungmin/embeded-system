9731c453a168d8c97f25a63354099ca01fc05cc2
Myoungmin <miung2@hanmail.net>
2019-12-12 08:28:58 +0900

    게임보드 설정 완료

--- Tetris_u8g2/Tetris_u8g2/main.c
+++ Tetris_u8g2/Tetris_u8g2/main.c
@@ -1,39 +1,7 @@
-/*
-  main.c
-	
-  AVR Test Project
-  This project will use 4-Wire SW SPI
-  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
-  Copyright (c) 2018, olikraus@gmail.com
-  All rights reserved.
-  Redistribution and use in source and binary forms, with or without modification, 
-  are permitted provided that the following conditions are met:
-  * Redistributions of source code must retain the above copyright notice, this list 
-    of conditions and the following disclaimer.
-    
-  * Redistributions in binary form must reproduce the above copyright notice, this 
-    list of conditions and the following disclaimer in the documentation and/or other 
-    materials provided with the distribution.
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
-	
-*/
-
-
 #include <avr/io.h>
 #include "u8g2.h"
 #include <util/delay.h>
+#include <math.h>
 
 #define DISPLAY_CLK_DIR DDRD
 #define DISPLAY_CLK_PORT PORTD
@@ -42,9 +10,6 @@
 #define DISPLAY_DATA_DIR DDRD
 #define DISPLAY_DATA_PORT PORTD
 #define DISPLAY_DATA_PIN 1
-
-
-
 
 #define P_CPU_NS (1000000000UL / F_CPU)
 
@@ -123,26 +88,102 @@ uint8_t u8x8_avr_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void
 	return 1;
 }
 
+
+void u8g2_prepare(void) {
+	u8g2_SetFont(&u8g2, u8g2_font_6x10_tf);
+	u8g2_SetFontRefHeightExtendedText(&u8g2);
+	u8g2_SetDrawColor(&u8g2, 1);
+	u8g2_SetFontPosTop(&u8g2);
+	u8g2_SetFontDirection(&u8g2, 0);
+
+}
+
+
+unsigned short tetriminos[7][4] = {
+
+	{0x6600, 0x6600, 0x6600, 0x6600},       // O
+	{0xF000, 0x4444, 0xF000, 0x4444},        // I
+	{0x6C00, 0x8C40, 0x6C00, 0x8C40},    // S
+	{0xC600, 0x4C80, 0xC600, 0x4C80},    // Z
+	{0xE400, 0x8C80, 0x4E00, 0x4C40},    // T
+	{0x2E00, 0xC440, 0xE800, 0x88C0},    // L
+	{0x8E00, 0x44C0, 0xE200, 0xC880},    // J
+
+};
+
+
+
+unsigned long int game_board[64];
+
+unsigned char shape;                  // 테트리미노스의 7가지 모양
+unsigned char pattern;                 // 테트리미노스의 4가지 패턴
+unsigned char cur_line;               // 테트리니노스의 현재 라인
+unsigned char cur_col;                // 테트리니노스의 현재 칸
+unsigned short temp_line[4];          // 테트리미노스 라인 임시 저장소
+
+
+ void NewTetriminos(void)
+ {
+	shape = TCNT0 % 7;    // 단순한 블럭 랜덤 생성 (여기서 TMR2는 dsPIC33F256GP710 Timer2 레지스터 값 임)
+	pattern = 0;                // 패턴은 초기 패턴 값으로 설정
+
+	temp_line[0] = (tetriminos[shape][pattern] & 0xF000) >> 6;
+	temp_line[1] = (tetriminos[shape][pattern] & 0x0F00) >> 2;
+	temp_line[2] = (tetriminos[shape][pattern] & 0x00F0) << 2;
+	temp_line[3] = (tetriminos[shape][pattern] & 0x000F) << 6;
+
+	cur_line = 0;                 // 테트리미노스 현재 라인 (최상위 라인)
+	cur_col = 9;                // 테트리미노스의 현재 칸  (그림 상 붉은 색으로 하이라이트 된 부분이 위치할 곳)
+
+
+ }
+
+ signed char Collision(void)
+ {
+
+	 if( ((game_board[cur_line] & temp_line[0]) != 0) | ((game_board[cur_line + 1] & temp_line[1]) != 0) |
+	 ((game_board[cur_line + 2] & temp_line[2]) != 0) | ((game_board[cur_line + 3] & temp_line[3]) != 0) )
+	 return -1;         // 충돌
+	 else
+
+	 return 0;  // 충돌 없음
+ }
+
+
+
+
 int main(void)
 {
-	/*
-		Select a setup procedure for your display from here: https://github.com/olikraus/u8g2/wiki/u8g2setupc
-		1. Arg: Address of an empty u8g2 structure
-		2. Arg: Usually U8G2_R0, others are listed here: https://github.com/olikraus/u8g2/wiki/u8g2reference#carduino-example
-		3. Arg: Protocol procedure (u8g2-byte), list is here: https://github.com/olikraus/u8g2/wiki/Porting-to-new-MCU-platform#communication-callback-eg-u8x8_byte_hw_i2c
-		4. Arg: Defined in this code itself (see above)
-	*/
+	
 	u8g2_Setup_ssd1306_i2c_128x64_noname_f( &u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_avr_gpio_and_delay );
 	u8g2_SetI2CAddress(&u8g2, 0x78);
 	u8g2_InitDisplay(&u8g2);
 	u8g2_SetPowerSave(&u8g2, 0);
 	
-	/* full buffer example, setup procedure ends in _f */
-	u8g2_ClearBuffer(&u8g2);
-	u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
-	u8g2_DrawStr(&u8g2, 1, 18, "U8g2 on AVR");
-	u8g2_SendBuffer(&u8g2);
-		
-	while(1){
+	u8g2_prepare();
+	
+
+	for (int i = 0; i < 63; i++ ) game_board[i] = 0xF0000F;   
+	game_board[63] = 0xFFFFFF;                              
+
+
+	while(1)
+	{
+		for(int i = 0; i < 24; i++)
+		{
+			for(int j = 0; j < 64; j++)
+			{
+				if((game_board[j] & ((unsigned long int)1<< i)) != 0)
+				{
+					u8g2_DrawPixel(&u8g2, 2 * j, 16 + 2 * i);
+					u8g2_DrawPixel(&u8g2, 2 * j, 16 + 2 * i + 1);
+					u8g2_DrawPixel(&u8g2, 2 * j + 1, 16 + 2 * i);
+					u8g2_DrawPixel(&u8g2, 2 * j + 1, 16 + 2 * i + 1);
+				}
+			}
+		}
+		u8g2_DrawPixel(&u8g2, 127, 63);
+		u8g2_SendBuffer(&u8g2);
+		u8g2_ClearBuffer(&u8g2);
 	}
 }
